32951f841f1917c78dd2e0b84f31fb78be1e76bc
Mann Garg <gargmann963@gmail.com>
2026-03-18 15:51:08 +0530

    Added .gitignore and some other things

---
+++ JavaScipt!/app.js
@@ -0,0 +1,35 @@
+console.log("hi js!");
+console.log("Javascript what are you doing");
+
+console.log("Bye bro!")
+console.log(10/3)
+console.log(1 + "2");
+// 1 2. console.log("1" + 2 + 3);
+// 1 3. console.log(1 + 2 + "3");
+// 1 4. console.log("10" + 5);
+// 1 5. console.log(5 + +"5");
+// 1 6. console.log("5" - "2");
+// 1 7. console.log("10" * "2");
+// 1 8. console.log("5" - true);
+// 1 9. console.log(true + true);
+// 1 10. console.log(null + 1);
+// 1 11. console.log(undefined + 1);
+// 1 12. console.log("5" == 5);
+// 1 13. console.log("5" === 5);
+// 1 14. console.log(0 == false);
+// 1 15. console.log(0 === false);
+// 1 16. console.log(+"5");
+// 1 17. console.log(+true);
+// 1 18. console.log(+[]);
+// 1 19. console.log([] + []);
+// 1 20. console.log([] == false);
+// 1 21. console.log([0] == false);
+// 1 22. console.log({} == {});
+// 1 23. console.log(1 + true + "5");
+// 1 24. console.log("5" - true + null);
+// 1 25. console.log(!!"false");
+// 1 26. console.log(0.1 + 0.2 == 0.3);
+// 1 27. console.log(NaN == NaN);
+// 1 28. console.log(" " == 0);
+// 1 29. console.log(null >= 0);
+// 1 30. console.log(null == 0);
