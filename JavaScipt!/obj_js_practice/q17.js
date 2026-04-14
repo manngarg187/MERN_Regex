44dacef67b2543538adb460b650001e57b61f63e
Mann Garg <gargmann963@gmail.com>
2026-04-14 20:39:51 +0530

    Solved Q17 also! - Finally Obj sheet completed

--- JavaScipt!/obj_js_practice/q17.js
+++ JavaScipt!/obj_js_practice/q17.js
@@ -8,6 +8,31 @@ let user = {
       area: "Rajpur Road",
       pincode: 248001
     }
-  }
+  },
+  age: 15
 };
+
+function flatten(obj, parentKey = "", result = {}) {
+  for (let key in obj) {
+    let newKey = parentKey ? `${parentKey}.${key}` : key;
+
+    if (typeof obj[key] !== "object" || obj[key] === null) {
+      result[newKey] = obj[key];
+    } else {
+      flatten(obj[key], newKey, result);
+    }
+  }
+  return result;
+}
+
+console.log(flatten(user));
+
+// Output :
+// {
+//   name: 'Switch',
+//   'address.city': 'Dehradun',
+//   'address.location.area': 'Rajpur Road',
+//   'address.location.pincode': 248001,
+//   age: 15
+// }
 
